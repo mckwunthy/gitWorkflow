1177992b7c0e96e27a6422a45b7f8f4a6c552a6a
Kamenan Aristide <universite.cotedivoire@gmail.com>
2024-03-25 00:20:20 +0000

    add comment on function

--- javascript/localstorage.js
+++ javascript/localstorage.js
@@ -1,1 +1,6 @@
-//localstorage js file : 
+//localstorage js file :
+//this file contain all functions need to manage data save in indexDB
+// addData(data)
+//deleteData(key)
+//updateData(date, dbName)
+//getDataAll()
